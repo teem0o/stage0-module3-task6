8bcc0796e94f3fe9d911b84d1b2bbd3163a89959
temo <chxaidzetemo@gmail.com>
2022-07-07 21:10:34 +0400

    FloatTypeCasting

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        long first=1000;
+        int second= (int) first;
+        System.out.println(second);
     }
 }
